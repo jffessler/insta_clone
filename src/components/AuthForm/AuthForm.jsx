8256f1ee8819ab265f7f6269892ce5a2242230ee
John Fessler <jffessler@gmail.com>
2024-01-17 20:57:16 -0800

    finished auth page UI design

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -1,10 +1,10 @@
-import { Box, Button, Image, Input, VStack } from "@chakra-ui/react"
+import { Box, Button, Flex, Image, Input, Text, VStack } from "@chakra-ui/react"
 import { useState } from "react";
 
 const AuthForm = () => {
     const [isLogin, setIsLogin] = useState(true)
     return(
-        // <>
+        <>
             <Box border={"1px solid grey"} borderRadius={4} padding={5}>
                 <VStack spacing={4}>
                     <Image src="/logo.png" h={24} cursor={"pointer"} alt="Instagram" />
@@ -29,9 +29,34 @@ const AuthForm = () => {
                     <Button w={"full"} colorScheme="blue" size={"sm"} fontSize={14}>
                         {isLogin? "Log In" : "Sign Up"}
                     </Button>
+
+                    {/* OR Text */}
+                    <Flex alignItems={"center"} justifyContent={"center"} my={4} gap={1} w={"full"}>
+                        <Box flex={2} h={"1px"} bg={"gray.400"} />
+                        <Text mx={1} color={"white"}>OR</Text>
+                        <Box flex={2} h={"1px"} bg={"gray.400"} />
+                    </Flex>
+
+                    <Flex alignItems={"center"} justifyContent={"center"} cursor={"pointer"}>
+                        <Image src="/google.png" w={5} alt="Google logo"/>
+                        <Text mx={"2"} color={"blue.500"}>
+                            Log in with Google
+                        </Text>
+                    </Flex>
                 </VStack>
             </Box>
-        // </>
+
+            <Box border={"1px solid gray"} borderRadius={4} padding={5}>
+                <Flex alignItems={"center"} justifyContent={"center"}>
+                    <Box mx={2} fontSize={14}>
+                        {isLogin ? "Don't have an account?" : "Already have an account?"}
+                    </Box>
+                    <Box onClick={() => setIsLogin(!isLogin)} color={"blue.500"} cursor={"pointer"}>
+                        {isLogin ? "Sign Up" : "Log In"}
+                    </Box>
+                </Flex>  
+            </Box>
+        </>
     );
 };
 
