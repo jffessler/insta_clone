937154f7df52b3a7bfdb3755ab22f9ece39e9f1f
John Fessler <jffessler@gmail.com>
2024-01-17 18:24:18 -0800

    continued working on auth page, setting up for user information

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -1,7 +1,38 @@
+import { Box, Button, Image, Input, VStack } from "@chakra-ui/react"
+import { useState } from "react";
+
 const AuthForm = () => {
-  return (
-    <div>AuthForm</div>
-  )
-}
+    const [isLogin, setIsLogin] = useState(true)
+    return(
+        // <>
+            <Box border={"1px solid grey"} borderRadius={4} padding={5}>
+                <VStack spacing={4}>
+                    <Image src="/logo.png" h={24} cursor={"pointer"} alt="Instagram" />
+                    <Input 
+                        placeholder="Email"
+                        fontSize={14}
+                        type="email"
+                    />
+                    <Input 
+                        placeholder="Password"
+                        fontSize={14}
+                        type="password"
+                    />
+                    {!isLogin ? (
+                        <Input 
+                        placeholder="Confirm Password"
+                        fontSize={14}
+                        type="password"
+                    />
+                    ) : null}
+
+                    <Button w={"full"} colorScheme="blue" size={"sm"} fontSize={14}>
+                        {isLogin? "Log In" : "Sign Up"}
+                    </Button>
+                </VStack>
+            </Box>
+        // </>
+    );
+};
 
 export default AuthForm
