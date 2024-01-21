f051044844d57f302f75c5eb4ef6a37a5d0ed91e
John Fessler <jffessler@gmail.com>
2024-01-20 23:57:00 -0800

    building template page for all content pages

---
+++ src/Layouts/PageLayout/PageLayout.jsx
@@ -0,0 +1,13 @@
+import { Flex } from "@chakra-ui/react"
+
+const PageLayout = () => {
+  return (
+    <Flex>  
+        {/* Side bar on the left */}
+
+        {/* Page content on the right */}
+    </Flex>
+  )
+}
+
+export default PageLayout
