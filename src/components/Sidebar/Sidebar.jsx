dc7f78533862118f7bb836d2fedb21828bba02b4
John Fessler <jffessler@gmail.com>
2024-01-21 19:09:44 -0800

    beginning to develp the home page

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,23 +1,78 @@
-import { Box, Flex, Link } from "@chakra-ui/react";
-import { Link as RouterLink} from "react-router-dom";
+import { Avatar, Box, Flex, Link } from "@chakra-ui/react";
+import { Link as RouterLink } from "react-router-dom";
+import {
+  CreatePostLogo,
+  InstagramLogo,
+  InstagramMobileLogo,
+  NotificationsLogo,
+  SearchLogo,
+} from "../../assets/constants";
+
+import { AiFillHome } from "react-icons/ai";
 
 const Sidebar = () => {
-  return <Box
-    height={"100vh"}
-    borderRight={"1px solid"}
-    borderColor={"whilteAlpha.300"}
-    py={8}
-    position={"sticky"}
-    top={0}
-    left={0}
-    px={{base:2,md:4}}
-  >
-    <Flex Link direction={"column"} gap={10} w={"full"} height={"full"}>
-        <Link to={"/"} as={RouterLink} pl={2} display={{base:"none",md:"block"}} cursor={"pointer"}>
-            <InstagramLogo />
+  const sidebarItems = [
+    {
+      icon: <AiFillHome size={25} />,
+      text: "Home",
+      link: "/",
+    },
+    {
+      icon: <SearchLogo />,
+      text: "Search",
+    },
+    {
+      icon: <NotificationsLogo />,
+      text: "Notifications",
+    },
+    {
+      icon: <CreatePostLogo />,
+      text: "Create",
+    },
+    {
+      icon: <Avatar size={"sm"} name="Burak Orkmez" src="/profilepic.png" />,
+      text: "Profile",
+      link: "/asaprogrammer",
+    },
+  ];
+  return (
+    <Box
+      height={"100vh"}
+      borderRight={"1px solid"}
+      borderColor={"whilteAlpha.300"}
+      py={8}
+      position={"sticky"}
+      top={0}
+      left={0}
+      px={{ base: 2, md: 4 }}
+    >
+      <Flex Link direction={"column"} gap={10} w={"full"} height={"full"}>
+        <Link
+          to={"/"}
+          as={RouterLink}
+          pl={2}
+          display={{ base: "none", md: "block" }}
+          cursor={"pointer"}
+        >
+          <InstagramLogo />
         </Link>
-    </Flex>
-    </Box>;
+        <Link
+          to={"/"}
+          as={RouterLink}
+          p={2}
+          display={{ base: "block", md: "none" }}
+          borderRadius={6}
+          _hover={{
+            bg: "whitealpha.200",
+          }}
+          w={10}
+          cursor={"pointer"}
+        >
+          <InstagramMobileLogo />
+        </Link>
+      </Flex>
+    </Box>
+  );
 };
 
 export default Sidebar;
