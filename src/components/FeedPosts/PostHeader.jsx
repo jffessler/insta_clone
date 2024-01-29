ebc03d7125c83ebcaf1953c761f66460f52c33cc
John Fessler <jffessler@gmail.com>
2024-01-28 17:34:03 -0800

    continue developing homepage UI

--- src/components/FeedPosts/PostHeader.jsx
+++ src/components/FeedPosts/PostHeader.jsx
@@ -1,5 +1,23 @@
+import { Avatar, Box, Flex } from "@chakra-ui/react";
+
 const PostHeader = () => {
-  return <div>PostHeader</div>;
+  return (
+    <Flex
+      justifyContent={"space-between"}
+      alignItems={"center"}
+      w={"full"}
+      my={2}
+    >
+      <Flex alignItems={"center"} gap={2}>
+        <Avatar src="/img1.png" alt="user profile pic" size={"sm"} />
+        <Flex fontSize={12} fontWeight={"bold"} gap="2">
+          asaprogrammer
+          <Box color={"gray.500"}>· 1w</Box>
+        </Flex>
+      </Flex>
+      <Box></Box>
+    </Flex>
+  );
 };
 
 export default PostHeader;
