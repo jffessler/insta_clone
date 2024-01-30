6bfdfbbcb5bea41e80005cfd26831d4736daa768
John Fessler <jffessler@gmail.com>
2024-01-29 18:50:32 -0800

    working on homepage post footer

--- src/components/FeedPosts/PostFooter.jsx
+++ src/components/FeedPosts/PostFooter.jsx
@@ -1,5 +1,13 @@
+import { Box, Flex } from "@chakra-ui/react";
+
 const PostFooter = () => {
-  return <div>PostFooter</div>;
+  return (
+    <>
+      <Flex alignItems={"center"} gap={4} w={"full"} pt={0} mb={2} mt={"auto"}>
+        <Box></Box>
+      </Flex>
+    </>
+  );
 };
 
 export default PostFooter;
