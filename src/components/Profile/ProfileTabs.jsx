a45ab69b05369f934fe69e2b1022eadeca9b33c7
John Fessler <jffessler@gmail.com>
2024-02-06 19:06:40 -0800

    working on profile page

--- src/components/Profile/ProfileTabs.jsx
+++ src/components/Profile/ProfileTabs.jsx
@@ -1,5 +1,22 @@
+import { Box, Flex } from "@chakra-ui/react";
+// import { BsGrid3x3 } from "react-icons/bs";
+
 const ProfileTabs = () => {
-  return <div>ProfileTabs</div>;
+  return (
+    <Flex
+      w={"full"}
+      justifyContent={"center"}
+      gap={{ base: 4, sm: 10 }}
+      textTransform={"uppercase"}
+      fontWeight={"bold"}
+    >
+      <Flex borderTop={"1px solid white"}>
+        <Box fontSize={20}>{/* <BsGrid3x3 /> */}</Box>
+      </Flex>
+      <Flex></Flex>
+      <Flex></Flex>
+    </Flex>
+  );
 };
 
 export default ProfileTabs;
