942333c7e8b1031b5be0a71631b2c575a080abe8
John Fessler <jffessler@gmail.com>
2024-02-06 21:37:20 -0800

    finished profile page tabs

--- src/components/Profile/ProfileTabs.jsx
+++ src/components/Profile/ProfileTabs.jsx
@@ -1,5 +1,5 @@
-import { Box, Flex } from "@chakra-ui/react";
-// import { BsGrid3x3 } from "react-icons/bs";
+import { Box, Flex, Text } from "@chakra-ui/react";
+import { BsBookmark, BsGrid3X3, BsSuitHeart } from "react-icons/bs";
 
 const ProfileTabs = () => {
   return (
@@ -10,11 +10,38 @@ const ProfileTabs = () => {
       textTransform={"uppercase"}
       fontWeight={"bold"}
     >
-      <Flex borderTop={"1px solid white"}>
-        <Box fontSize={20}>{/* <BsGrid3x3 /> */}</Box>
+      <Flex
+        borderTop={"1px solid white"}
+        alignItems={"center"}
+        p={3}
+        gap={1}
+        cursor={"pointer"}
+      >
+        <Box fontSize={20}>
+          <BsGrid3X3 />
+        </Box>
+        <Text fontSize={12} display={{ base: "none", sm: "block" }}>
+          Posts
+        </Text>
       </Flex>
-      <Flex></Flex>
-      <Flex></Flex>
+
+      <Flex alignItems={"center"} p={3} gap={1} cursor={"pointer"}>
+        <Box fontSize={20}>
+          <BsBookmark />
+        </Box>
+        <Text fontSize={12} display={{ base: "none", sm: "block" }}>
+          Saved
+        </Text>
+      </Flex>
+
+      <Flex alignItems={"center"} p={3} gap={1} cursor={"pointer"}>
+        <Box fontSize={20}>
+          <BsSuitHeart fontWeight={"bold"} />
+        </Box>
+        <Text fontSize={12} display={{ base: "none", sm: "block" }}>
+          Likes
+        </Text>
+      </Flex>
     </Flex>
   );
 };
