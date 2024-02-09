0e1888ca0f3f2ebdaa6bdd0494ead15e97eae542
John Fessler <jffessler@gmail.com>
2024-02-08 18:58:32 -0800

    developing profile page post ui

--- src/components/Profile/ProfilePosts.jsx
+++ src/components/Profile/ProfilePosts.jsx
@@ -1,5 +1,31 @@
+import { Box, Grid, Skeleton, VStack } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+
 const ProfilePosts = () => {
-  return <div>ProfilePosts</div>;
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
+
+  return (
+    <Grid
+      templateColumns={{ sm: "repate(1, 1fr)", md: "repeat(3, 1fr)" }}
+      gap={1}
+      columnGap={1}
+    >
+      {isLoading &&
+        [0, 1, 2, 3, 4, 5].map((_, idx) => (
+          <VStack key={idx} alignItems={"flex-start"} gap={4}>
+            <Skeleton w={"full"}>
+              <Box h={"300px"}>contents wrapped</Box>
+            </Skeleton>
+          </VStack>
+        ))}
+    </Grid>
+  );
 };
 
 export default ProfilePosts;
