40621705161a3d969585932d629972f70d0e657f
John Fessler <jffessler@gmail.com>
2024-03-03 19:16:31 -0800

    continuing auth state control

---
+++ src/store/authStore.js
@@ -0,0 +1,8 @@
+import { create } from "zustand";
+
+const useAuthStore = create((set) => ({
+  user: null,
+  login: (user) => set({ user }),
+  logout: () => set({ user: null }),
+  setUser: (user) => set({ user }),
+}));
