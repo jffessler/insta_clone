767b8f9d9d3e4f55db7faccfba0cda1e80e4736b
John Fessler <jffessler@gmail.com>
2024-03-12 21:21:36 -0700

    optimizing ui for non-auth users

--- src/Layouts/PageLayout/PageLayout.jsx
+++ src/Layouts/PageLayout/PageLayout.jsx
@@ -1,28 +1,38 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Flex, Spinner } from "@chakra-ui/react";
 import Sidebar from "../../components/Sidebar/Sidebar";
 import { useLocation } from "react-router-dom";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../../firebase/firebase";
+import Navbar from "../../components/Navbar/Navbar";
 
 // Instead of adding the Sidebar component to every pageXOffset, we can add it only once to the
 // PageLayout component and wrap the children with it. This way, we can have a sidebar on every page except the AuthPage
 
 const PageLayout = ({ children }) => {
   const { pathname } = useLocation();
-  const [user, loading, error] = useAuthState(auth);
+  const [user, loading] = useAuthState(auth);
   const canRenderSidebar = pathname !== "/auth" && user;
+  const canRenderNavbar = !user && !loading && pathname !== "/auth";
+
+  const checkingUserIsAuth = !user && loading;
+  if (checkingUserIsAuth) return <PageLayoutSpinner />;
 
   return (
-    <Flex>
+    <Flex flexDir={canRenderNavbar ? "column" : "row"}>
       {/* Side bar on the left */}
       {canRenderSidebar ? (
         <Box w={{ base: "70px", md: "240px" }}>
           <Sidebar />
         </Box>
       ) : null}
-
+      {/* Navbar */}
+      {canRenderNavbar ? <Navbar /> : null}
       {/* Page content on the right */}
-      <Box flex={1} w={{ base: "calc(100%-70px", md: "calc(100%-240px)" }}>
+      <Box
+        flex={1}
+        w={{ base: "calc(100%-70px", md: "calc(100%-240px)" }}
+        mx={"auto"}
+      >
         {children}
       </Box>
     </Flex>
@@ -30,3 +40,16 @@ const PageLayout = ({ children }) => {
 };
 
 export default PageLayout;
+
+const PageLayoutSpinner = () => {
+  return (
+    <Flex
+      flexDir="column"
+      h="100vh"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Spinner size="xl" />
+    </Flex>
+  );
+};
